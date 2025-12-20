f0d7b8f015f849efef01b344e359151dd375a617
joe0106 <beebub0106@gmail.com>
2025-12-20 15:47:20 +0800

    初始化專案，參考zh424的鐵人賽的文章

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from setting.config import get_settings
+
+app = FastAPI()
+
+@app.get("/")
+def hello_world():
+    return "hello world !!! @@"
+
+@app.get("/users/{user_id}")
+def get_users(user_id: int, query: str = None):
+    return {"user_id": user_id, "query": query}
+
+@app.get("/infor")
+def get_infor():
+    settings = get_settings()
+    return {
+        "app_name": settings.app_name,
+        "author": settings.author,
+        "app_mode": settings.app_mode,
+        "port": settings.port,
+        "reload": settings.reload,
+        "database_url": settings.database_url
+    }
