1ef189fac1b9314c5ce876dc49e65979e056c7ca
joe0106 <beebub0106@gmail.com>
2025-12-21 15:01:55 +0800

    1. 實作CRUD 2. 機敏資訊獨立Schema 3. 使用HTTP Exception

--- main.py
+++ main.py
@@ -1,16 +1,76 @@
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, HTTPException
 from setting.config import get_settings
 
+from schemas import users as UserSchema
+from schemas import items as ItemSchema
+from database.fake_db import get_db
+
 app = FastAPI()
+
+fake_db = get_db()
 
 @app.get("/")
 def hello_world():
     return "hello world !!! @@"
 
-@app.get("/users/{user_id}")
-def get_users(user_id: int, query: str = None):
-    return {"user_id": user_id, "query": query}
+#Users
+@app.get("/users", response_model=List[UserSchema.UserRead])
+def get_users(query: str = None):
+    return fake_db["users"]
 
+@app.get("/users/{user_id}", response_model=UserSchema.UserRead)
+def get_user_by_id(user_id: int, query: str = None):
+    for user in fake_db["users"]:
+        if user["id"] == user_id:
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
+
+@app.post("/users", response_model=UserSchema.UserCreateResponse)
+def create_users(newUser: UserSchema.UserCreate):
+    for user in fake_db["users"]:
+        if newUser.id == user["id"]:
+            raise HTTPException(status_code=409, detail="User already exist")
+    fake_db["users"].append(newUser.model_dump())
+    return newUser
+
+@app.delete("/users/{user_id}")
+def delete_users(user_id: int):
+    for user in fake_db["users"]:
+        if user["id"] == user_id:
+            fake_db["users"].remove(user)
+            return f"user_id: {user["id"]} deleted"
+    raise HTTPException(status_code=404, detail="User not found")
+
+#Items
+@app.get("/items", response_model=List[ItemSchema.ItemRead])
+def get_items(query: str = None):
+    return fake_db["items"]
+
+@app.get("/items/{item_id}", response_model=ItemSchema.ItemRead)
+def get_item_by_id(item_id: int, query: str = None):
+    for item in fake_db["items"]:
+        if item["id"] == item_id:
+            return item
+    raise HTTPException(status_code=404, detail="Item not found")
+
+@app.post("/items", response_model=ItemSchema.ItemCreate)
+def create_item(newItem: ItemSchema.ItemCreate):
+    for item in fake_db["items"]:
+        if newItem.id == item["id"]:
+            raise HTTPException(status_code=409, detail="Item already exist")
+    fake_db["items"].append(newItem.model_dump())
+    return newItem
+
+@app.delete("/items/{item_id}")
+def delete_items(item_id: int):
+    for item in fake_db["item"]:
+        if item["id"] == item_id:
+            fake_db["item"].remove(item_id)
+            return item
+    raise HTTPException(status_code=404, detail="Item not found")
+
+#Others
 @app.get("/infor")
 def get_infor():
     settings = get_settings()
---
+++ schemas/users.py
@@ -0,0 +1,23 @@
+from datetime import date
+from pydantic import BaseModel
+from typing import Optional
+
+class UserBase(BaseModel):
+    id: int
+
+class UserCreate(UserBase):
+    password: str
+    name: str
+    avatar: Optional[str] = None
+    age: int
+    email: str
+    birthday: date
+
+class UserRead(UserBase):
+    name: str
+    email: str
+    avatar: Optional[str] = None
+
+class UserCreateResponse(UserBase):
+    name: str
+    email: str
