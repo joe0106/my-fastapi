fc91c02a61cef0847903734122c020f5dd21c82d
joe0106 <beebub0106@gmail.com>
2025-12-28 15:06:23 +0800

    1. 應用程式startup時初始化db，並使用declarative_base初始化table 2. Users、Items繼承sqlalchemy的base type 3. /api/infor 實作測試寫入及讀取的方法

--- database/generic.py
+++ database/generic.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from models.items import Item
+from models.users import User
 from setting.config import get_settings
 
 
@@ -19,3 +21,6 @@ Base = declarative_base()
 
 def get_db():
     return SessionLocal()
+
+def init_db():
+    Base.metadata.create_all(bind=engine, tables=[User.__table__, Item.__table__])
---
+++ models/users.py
@@ -0,0 +1,43 @@
+import hashlib
+
+from typing import Optional
+
+from models.base import Base, BaseType
+from datetime import date
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+from sqlalchemy import Date
+
+class User(Base):
+    __tablename__ = "User"
+    id:Mapped[BaseType.int_primary_key]
+    password:Mapped[BaseType.str_50]
+    name:Mapped[BaseType.str_30]
+    age:Mapped[int]
+    avatar:Mapped[BaseType.optional_str_100]
+    birthday:Mapped[date] = mapped_column(Date)
+    email:Mapped[BaseType.str_50]
+    create_time:Mapped[BaseType.update_time]
+
+    items:Mapped[list["Item"]] \
+        = relationship("Item", 
+            back_populates="user", 
+            cascade="all, delete-orphan", 
+            lazy="select", 
+            order_by="Item.name"
+        )
+    
+    def __init__(self, password:str, name:str, age:int, avatar:Optional[str], birthday:date, email:str) -> None:
+        self.password = hashlib.md5(password.encode()+b'secret').hexdigest()
+        self.name = name
+        self.age = age
+        self.avatar = avatar
+        self.birthday = birthday
+        self.email = email
+
+    # 通常用來顯示簡單資訊給使用者；有__str__時會優先__repr__顯示
+    def __str__(self):
+        return f"User(id={self.id}, name={self.name}, email={self.email})"
+
+    # Debug、詳細顯示資訊使用
+    def __repr__(self) -> str:
+        return f"User(id={self.id}, password=******, name={self.name}, age={self.age}, avatar={self.avatar}, birthday={self.birthday}, email={self.email})"
