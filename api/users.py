9285dfa12872bf66a08e9f726af85350090de6c7
joe0106 <beebub0106@gmail.com>
2026-01-11 14:44:59 +0800

    新增get_current_user Depends到delete, update相關api

--- api/users.py
+++ api/users.py
@@ -1,7 +1,10 @@
 from typing import List
 from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer
 
 from auth.passwd import get_password_hash
+from auth.jwt import verify_access_token
+from auth.utils import get_current_user
 from schemas import users as UserSchema
 from api.depends import check_user_id, pagination_parms
 #from api.depends import test_verify_token
@@ -15,6 +18,8 @@ router = APIRouter(
 )
 
 UserCrud = UserCrudManager()
+
+Exception403 = HTTPException(status_code=403, detail="Permission denied")
 
 @router.get("/users", 
          response_model=List[UserSchema.UserRead],
@@ -52,20 +57,37 @@ async def create_users(newUser: UserSchema.UserCreate):
     newUser.password = get_password_hash(newUser.password)
 
     user = await UserCrud.create_user(newUser)
-    return vars(user)
+    return user
 
 @router.put("/users/{user_id}}", response_model=UserSchema.UserUpdateResponse)
-async def update_user(newUser: UserSchema.UserUpdate, user_id: int = Depends(check_user_id)):
+async def update_user(
+    newUser: UserSchema.UserUpdate, 
+    user_id: int = Depends(check_user_id),
+    user = Depends(get_current_user)):
+    if user.id != user_id:
+        raise Exception403
+    
     await UserCrud.update_user(user_id, newUser)
     return newUser
 
 @router.put("/users/{user_id}/password", response_model=UserSchema.UserUpdateResponse)
-async def update_user_password(newUser: UserSchema.UserUpdate, user_id: int = Depends(check_user_id)):
+async def update_user_password(
+    newUser: UserSchema.UserUpdate, 
+    user_id: int = Depends(check_user_id),
+    user: UserSchema.CurrentUser = Depends(get_current_user)):
+    if user.id != user_id:
+        Exception403
+
     await UserCrud.update_user_password(user_id, newUser)
     return newUser
 
 @router.delete("/users/{user_id}")
-async def delete_users(user_id: int = Depends(check_user_id)):
+async def delete_users(
+    user_id: int = Depends(check_user_id),
+    user: UserSchema.CurrentUser = Depends(get_current_user)):
+    if user.id != user_id:
+        Exception403
+        
     await UserCrud.delete_users(user_id)
     return
 
